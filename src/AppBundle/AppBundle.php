e8eb21ff2d9f3cc563a9ac948ac4c1ae8f82b94a
Mikkel Ricky <rimi@aarhus.dk>
2017-07-12 11:01:22 +0200

    GAEST-28: Cleaned up

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -7,13 +7,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-    public function boot()
-    {
-        // Enable filter and inject container.
-        // @see http://stackoverflow.com/a/14650403
-        $em = $this->container->get('doctrine.orm.default_entity_manager');
-        $conf = $em->getConfiguration();
-        $conf->addFilter('filter_user_template', UserTemplateFilter::class);
-        $em->getFilters()->enable('filter_user_template')->setContainer($this->container);
-    }
 }
