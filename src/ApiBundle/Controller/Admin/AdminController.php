d08200d1eba483180517f8410462079262fbde7d
Mikkel Ricky <rimi@aarhus.dk>
2017-08-22 10:26:17 +0200

    GAEST-69: Added additional admin api methods

--- src/ApiBundle/Controller/Admin/AdminController.php
+++ src/ApiBundle/Controller/Admin/AdminController.php
@@ -206,80 +206,62 @@ class AdminController extends Controller
         return array_values($result);
     }
 
-    // /**
-    //  * @Rest\Get("/visit", name="api_visit_list")
-    //  */
-    // public function visitListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getVisits($request->query->all());
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visits",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     * @return array
+     */
+    public function getVisitsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisits($request->query->all());
 
-    //     return new JsonResponse($result);
-    // }
+        return $result;
+    }
 
-    // /**
-    //  * @Rest\Get("/visitor", name="api_visitor_list")
-    //  */
-    // public function visitorListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getVisitors($request->query->all());
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visitors",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     * @return array
+     */
+    public function getVisitorsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisitors($request->query->all());
 
-    //     return new JsonResponse($result);
-    // }
+        return $result;
+    }
 
-    // /**
-    //  * @Rest\Get("/identifier", name="api_identifier_list")
-    //  */
-    // public function identifierListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getIdentifiers($request->query->all());
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of identifiers",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     * @return array
+     */
+    public function getIdentifiersAction(Request $request)
+    {
+        $result = $this->aeosService->getIdentifiers($request->query->all());
 
-    //     return new JsonResponse($result);
-    // }
+        // header('Content-type: text/plain'); echo var_export([
+        //     $this->aeosService->getLastRequest(),
+        //     $this->aeosService->getLastResponse(),
+        // ], true); die(__FILE__.':'.__LINE__.':'.__METHOD__);
 
-    // /**
-    //  * @Rest\Post("/code/new", name="api_code_create")
-    //  */
-    // public function codeCreateAction(Request $request)
-    // {
-    //     $data = json_decode($request->getContent());
-
-    //     $user = $this->getUser();
-    //     $template = $this->entityManager->getRepository(Template::class)->find($data->template);
-    //     $startTime = new \DateTime($data->startTime);
-    //     $endTime = new \DateTime($data->endTime);
-
-    //     $code = new Code();
-    //     $code->setTemplate($template)
-    //         ->setStartTime($startTime)
-    //         ->setEndTime($endTime)
-    //         ->setCreatedBy($user);
-    //     $this->aeosHelper->createAeosIdentifier($code);
-    //     $this->entityManager->persist($code);
-    //     $this->entityManager->flush();
-
-    //     $result = [
-    //         'status' => 'ok',
-    //         'code' => $code->getIdentifier(),
-    //     ];
-
-    //     return new JsonResponse($result);
-    // }
-
-    // /**
-    //  * @Rest\Get("/code/{code}", name="api_code_show")
-    //  */
-    // public function codeInfoAction(Request $request, $code)
-    // {
-    //     $identifier = $this->aeosService->getIdentifierByBadgeNumber($code);
-    //     $visitor = $identifier ? $this->aeosService->getVisitorByIdentifier($identifier) : null;
-    //     $visit = $visitor ? $this->aeosService->getVisitByVisitor($visitor) : null;
-
-    //     $result = [
-    //         'identifier' => $identifier,
-    //         'visitor' => $visitor,
-    //         'visit' => $visit,
-    //     ];
-
-    //     return new JsonResponse($result);
-    // }
+        return $result;
+    }
 }
