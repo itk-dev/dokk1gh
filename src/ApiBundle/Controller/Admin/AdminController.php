1b282e39473bf7129e994bdba049219ba341bf1e
Mikkel Ricky <rimi@aarhus.dk>
2017-08-31 12:24:01 +0200

    Merge

--- src/ApiBundle/Controller/Admin/AdminController.php
+++ src/ApiBundle/Controller/Admin/AdminController.php
@@ -188,6 +188,120 @@ class AdminController extends Controller
         return $result;
     }
 
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visits",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getVisitsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisits($request->query->all());
+
+        return $result;
+    }
+
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visitors",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getVisitorsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisitors($request->query->all());
+
+        return $result;
+    }
+
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of identifiers",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getIdentifiersAction(Request $request)
+    {
+        $result = $this->aeosService->getIdentifiers($request->query->all());
+
+        return $result;
+    }
+
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visits",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getVisitsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisits($request->query->all());
+
+        return $result;
+    }
+
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of visitors",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getVisitorsAction(Request $request)
+    {
+        $result = $this->aeosService->getVisitors($request->query->all());
+
+        return $result;
+    }
+
+    /**
+     * @SWG\Tag(name="Miscellaneous")
+     * @SWG\Response(
+     *  response=200,
+     *  description="List of identifiers",
+     *  @SWG\Schema(
+     *    type="array"
+     *  )
+     * )
+     *
+     * @return array
+     */
+    public function getIdentifiersAction(Request $request)
+    {
+        $result = $this->aeosService->getIdentifiers($request->query->all());
+
+        return $result;
+    }
+
     private function searchAction(Request $request, string $method, array $keys)
     {
         $query = $request->query->get('query');
@@ -217,81 +331,4 @@ class AdminController extends Controller
 
         return array_values($result);
     }
-
-    // /**
-    //  * @Rest\Get("/visit", name="api_visit_list")
-    //  */
-    // public function visitListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getVisits($request->query->all());
-
-    //     return new JsonResponse($result);
-    // }
-
-    // /**
-    //  * @Rest\Get("/visitor", name="api_visitor_list")
-    //  */
-    // public function visitorListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getVisitors($request->query->all());
-
-    //     return new JsonResponse($result);
-    // }
-
-    // /**
-    //  * @Rest\Get("/identifier", name="api_identifier_list")
-    //  */
-    // public function identifierListAction(Request $request)
-    // {
-    //     $result = $this->aeosService->getIdentifiers($request->query->all());
-
-    //     return new JsonResponse($result);
-    // }
-
-    // /**
-    //  * @Rest\Post("/code/new", name="api_code_create")
-    //  */
-    // public function codeCreateAction(Request $request)
-    // {
-    //     $data = json_decode($request->getContent());
-
-    //     $user = $this->getUser();
-    //     $template = $this->entityManager->getRepository(Template::class)->find($data->template);
-    //     $startTime = new \DateTime($data->startTime);
-    //     $endTime = new \DateTime($data->endTime);
-
-    //     $code = new Code();
-    //     $code->setTemplate($template)
-    //         ->setStartTime($startTime)
-    //         ->setEndTime($endTime)
-    //         ->setCreatedBy($user);
-    //     $this->aeosHelper->createAeosIdentifier($code);
-    //     $this->entityManager->persist($code);
-    //     $this->entityManager->flush();
-
-    //     $result = [
-    //         'status' => 'ok',
-    //         'code' => $code->getIdentifier(),
-    //     ];
-
-    //     return new JsonResponse($result);
-    // }
-
-    // /**
-    //  * @Rest\Get("/code/{code}", name="api_code_show")
-    //  */
-    // public function codeInfoAction(Request $request, $code)
-    // {
-    //     $identifier = $this->aeosService->getIdentifierByBadgeNumber($code);
-    //     $visitor = $identifier ? $this->aeosService->getVisitorByIdentifier($identifier) : null;
-    //     $visit = $visitor ? $this->aeosService->getVisitByVisitor($visitor) : null;
-
-    //     $result = [
-    //         'identifier' => $identifier,
-    //         'visitor' => $visitor,
-    //         'visit' => $visit,
-    //     ];
-
-    //     return new JsonResponse($result);
-    // }
 }
---
+++ src/AppBundle/Command/AeosCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Service\AeosService;
+use Doctrine\ORM\EntityManagerInterface;
+
+class AeosCommand extends AbstractBaseCommand
+{
+    /** @var \Doctrine\ORM\EntityManagerInterface */
+    protected $entityManager;
+
+    /** @var \AppBundle\Service\AeosService */
+    protected $aeosService;
+
+    public function __construct(EntityManagerInterface $entityManager, AeosService $aeosService)
+    {
+        parent::__construct('app:aeos');
+        $this->entityManager = $entityManager;
+        $this->aeosService = $aeosService;
+    }
+
+    /**
+     * List identifiers.
+     *
+     * @command
+     */
+    protected function listIdentifiers()
+    {
+        $serializer = $this->getContainer()->get('jms_serializer');
+
+        $visitors = $this->getVisitors();
+        $visits = $this->getVisits($visitors);
+        $identifiers = $this->getIdentifiers($visitors);
+        $visitorId2visitId = [];
+        foreach ($visits as $visit) {
+            $visitorId2visitId[$visit->VisitorId] = $visit->Id;
+        }
+
+        foreach ($identifiers as $identifier) {
+            // $visitor = $identifier ? $this->aeosService->getVisitorByIdentifier($identifier) : null;
+            // $visit = $visitor ? $this->aeosService->getVisitByVisitor($visitor) : null;
+            $visitor = $visitors[$identifier->CarrierId];
+            $visit = $visits[$visitorId2visitId[$visitor->Id]];
+            $this->writeln(var_export([
+                'identifier' => $identifier,
+                'visitor' => $visitor,
+                'visit' => $visit,
+            ], true));
+        }
+    }
+
+    /**
+     * @command
+     */
+    protected function cleanUpIdentifiers()
+    {
+    }
+
+    private function getVisitors()
+    {
+        return $this->getItems('visitor', ['LastName' => 'dokk1gh']);
+    }
+
+    private function getVisits(array $visitors)
+    {
+        return array_filter($this->getItems('visit'), function ($visit) use ($visitors) {
+            return isset($visit->VisitorId) && isset($visitors[$visit->VisitorId]);
+        });
+    }
+
+    private function getIdentifiers(array $visitors)
+    {
+        return array_filter($this->getItems('identifier'), function ($identifier) use ($visitors) {
+            return isset($identifier->CarrierId) && isset($visitors[$identifier->CarrierId]);
+        });
+    }
+
+    private function getItems(string $name, array $query = [])
+    {
+        $methods = [
+            'identifier' => 'getIdentifiers',
+            'visit' => 'getVisits',
+            'visitor' => 'getVisitors',
+        ];
+        $method = $methods[$name];
+
+        $items = [];
+
+        $amount = 100;
+        $offset = 0;
+        $query += ['amount' => $amount, 'offset' => $offset];
+        while (true) {
+            $query['offset'] = $offset;
+            $result = $this->aeosService->{$method}($query);
+            foreach ($result as $item) {
+                $items[$item->Id] = $item;
+            }
+
+            if (count($result) < $amount) {
+                break;
+            }
+
+            $offset += $amount;
+        }
+
+        return $items;
+    }
+}
