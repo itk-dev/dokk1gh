b40f57b9c7b7cdb9052c239783dc416403a2c7f3
Mikkel Ricky <rimi@aarhus.dk>
2017-09-05 11:58:23 +0200

    GAEST-94: Fixed validation in user/template form

--- src/AppBundle/Validator/Constraints/AeosTemplateIdValidator.php
+++ src/AppBundle/Validator/Constraints/AeosTemplateIdValidator.php
@@ -18,7 +18,7 @@ class AeosTemplateIdValidator extends ConstraintValidator
 
     public function validate($value, Constraint $constraint)
     {
-        $template = $this->aeosService->getTemplate($value);
+        $template = $value ? $this->aeosService->getTemplate($value) : null;
         if (!$template) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ string }}', $value)
