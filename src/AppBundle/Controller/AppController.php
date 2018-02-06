cb07ca1437d094fd34db317b28ca2f0e1793362e
Mikkel Ricky <rimi@aarhus.dk>
2018-02-06 10:11:24 +0100

    GAEST-129: Added app controller and templates

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of Gæstehåndtering.
+ *
+ * (c) 2017–2018 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AppController.
+ *
+ * @Route("/app")
+ */
+class AppController extends Controller
+{
+    /**
+     * @Route("", name="app_code")
+     */
+    public function codeAction()
+    {
+        return $this->render('app/code/index.html.twig');
+    }
+
+    /**
+     * @Route("/card", name="app_card")
+     */
+    public function cardAction()
+    {
+        return $this->render('app/card/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('app/about/index.html.twig');
+    }
+}
