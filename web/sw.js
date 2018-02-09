768792afdf179841ad84e73426859c33b7c11095
Jesper Pedersen <jesper@yepz.dk>
2018-02-09 12:59:41 +0100

    GUEST-123: Update mainfest and add serviceworker file

---
+++ web/sw.js
@@ -0,0 +1,19 @@
+var CACHE_NAME = 'code2dokk1-v1';
+var urlsToCache = [
+  '/',
+  '/stylesheets/app.css',
+  '/static/js/jquery.min.js',
+  '/static/js/popper.min.js',
+  '/static/js/bootstrap.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
