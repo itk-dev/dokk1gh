97f0e9aed999d90ea54da0183328dcdf0857392e
Jesper Pedersen <jesper@yepz.dk>
2018-02-12 14:38:34 +0100

    GAEST-123: Hide urls for now

--- web/sw.js
+++ web/sw.js
@@ -1,19 +1,18 @@
 var CACHE_NAME = 'code2dokk1-v1';
 var urlsToCache = [
-  '/',
-  '/stylesheets/app.css',
-  '/static/js/jquery.min.js',
-  '/static/js/popper.min.js',
-  '/static/js/bootstrap.min.js'
+//   '/stylesheets/app.css',
+//   '/static/js/jquery.min.js',
+//   '/static/js/popper.min.js',
+//   '/static/js/bootstrap.min.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("Opened cache");
+            console.log(urlsToCache);
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
