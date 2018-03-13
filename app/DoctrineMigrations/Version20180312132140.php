1eed8df33819fea4df9e3cbc8d301ba79cbd8068
Mikkel Ricky <rimi@aarhus.dk>
2018-03-13 10:17:14 +0100

    SALG-232: Cleaned up database migration

--- app/DoctrineMigrations/Version20180312132140.php
+++ app/DoctrineMigrations/Version20180312132140.php
@@ -26,7 +26,6 @@ class Version20180312132140 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE itkdev_log_entry');
         $this->addSql('ALTER TABLE fos_user ADD gdpr_accepted_at DATETIME DEFAULT NULL');
     }
 
@@ -38,7 +37,6 @@ class Version20180312132140 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE itkdev_log_entry (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, message LONGTEXT NOT NULL COLLATE utf8_unicode_ci, context LONGTEXT NOT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:array)\', level SMALLINT NOT NULL, level_name VARCHAR(50) NOT NULL COLLATE utf8_unicode_ci, extra LONGTEXT NOT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:array)\', created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE fos_user DROP gdpr_accepted_at');
     }
 }
