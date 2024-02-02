dfcfea35e502666e728597a026fb9867e632bd88
Jesper Pedersen <jesper@yepz.dk>
2024-02-02 14:24:58 +0100

    607: Update fontawesome

--- assets/webapp/webapp.js
+++ assets/webapp/webapp.js
@@ -1,13 +1,39 @@
-// Scss
+// Styling
 import './scss/webapp.scss';
 
+// Fonts
+import '@fontsource/montserrat';
+
+// start the Stimulus application
+//import '../bootstrap';
+
+// jQuery
+const $ = require('jquery');
+
+console.log("TEST");
 // Fontawsome icons
+import '@fortawesome/fontawesome-svg-core';
+import {
+    faLockOpen,
+    faIdCard,
+    faInfoCircle,
+    faBan,
+    faWifi,
+    faCheckSquare,
+    faCircleNotch
+} from '@fortawesome/free-solid-svg-icons';
+library.add(
+    faLockOpen,
+    faIdCard,
+    faInfoCircle,
+    faBan,
+    faWifi,
+    faCheckSquare,
+    faCircleNotch
+);
 
-import { faAddressBook } from '@fortawesome/fontawesome-free'
-library.add(faAddressBook)
-
-// JS
-import "@fontsource/montserrat";
-import "add-to-homescreen"
-import "clipboard"
+// JavaScrips
+import 'add-to-homescreen';
+import 'clipboard';
 import './js/ios_stay_standalone';
+
