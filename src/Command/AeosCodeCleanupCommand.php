4fb4e6570bd46e923c5faaa7f2cada67158f1699
Mikkel Ricky <rimi@aarhus.dk>
2024-05-31 14:11:20 +0200

    608: Fixed command description

--- src/Command/AeosCodeCleanupCommand.php
+++ src/Command/AeosCodeCleanupCommand.php
@@ -20,7 +20,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'app:aeos:code-cleanup',
-    description: 'Create user'
+    description: 'Clean up expired codes'
 )]
 class AeosCodeCleanupCommand extends Command
 {
