38665f7494d7cdda2b0035c53c1cf537156131d5
Mikkel Ricky <rimi@aarhus.dk>
2025-11-07 13:44:01 +0100

    Added AEOS test command

---
+++ src/Command/AeosTestCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of Gæstehåndtering.
+ *
+ * (c) 2017–2024 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Command;
+
+use App\Service\AeosService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:aeos:test',
+)]
+class AeosTestCommand extends Command
+{
+    public function __construct(
+        private readonly AeosService $aeosService,
+    ) {
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $result = $this->aeosService->getTemplates();
+        $output->writeln(json_encode($result));
+
+        return static::SUCCESS;
+    }
+}
